a325d7e30a50eb3d20f9b3bc42291b6c88db5b67
kaushik1904 <kaushikprajapati1970@gmail.com>
2023-02-08 18:47:13 +0530

    half course done

---
+++ 08-02/angular-course-1-components/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+import { COURSES } from "../db-data";
+import { Course } from "./model/course";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  coreCourse = COURSES[0];
+  rxjsCourse = COURSES[1];
+  ngrxCourse = COURSES[2];
+
+  onCourseSelected(course: Course) {
+    console.log("course selected",course);
+  }
+}
