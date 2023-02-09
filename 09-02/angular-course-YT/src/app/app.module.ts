7bae3da887b0f18577bed5c6b52d3589484a79c3
kaushik1904 <kaushikprajapati1970@gmail.com>
2023-02-09 16:52:40 +0530

    component interaction => Input() & Output() for custom events

--- 09-02/angular-course-YT/src/app/app.module.ts
+++ 09-02/angular-course-YT/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { InterpolationComponent } from './interpolation/interpolation.component';
 import { BindingComponent } from './binding/binding.component';
 import { StructuralDirectivesComponent } from './structural-directives/structural-directives.component';
+import { ComponentInteractionComponent } from './component-interaction/component-interaction.component';
 
 @NgModule({
-  declarations: [AppComponent, InterpolationComponent, BindingComponent, StructuralDirectivesComponent],
+  declarations: [AppComponent, InterpolationComponent, BindingComponent, StructuralDirectivesComponent, ComponentInteractionComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
