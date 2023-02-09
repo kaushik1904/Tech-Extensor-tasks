b1de8fe361ce92a876aa68a9e37fcf15a6285ed1
kaushik1904 <kaushikprajapati1970@gmail.com>
2023-02-09 14:43:57 +0530

    binding & two way binding

--- 09-02/angular-course-YT/src/app/app.component.ts
+++ 09-02/angular-course-YT/src/app/app.component.ts
@@ -10,7 +10,33 @@ export class AppComponent {
 
   public name = 'kaushik';
 
-  getName(){
+  public successClass = 'text-success';
+  public hasError = true;
+
+  public multipleStyle = {
+    fontSize: '20px',
+    fontBold: '100px',
+    color: 'blue',
+  };
+
+  getName() {
     return this.name;
   }
+
+  public greeting = '';
+
+  onButtonClick(event: any) {
+    console.log(event);
+    this.greeting =
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, corrupti?';
+    console.log('btn click');
+  }
+
+  // Template Reference Variables
+  logMsg(value: any) {
+    console.log(value);
+  }
+
+  // two way binding
+  public value = '';
 }
