9b6e3f2d572af10604742ccff6fae9f303c4da45
kaushik1904 <kaushikprajapati1970@gmail.com>
2023-02-16 10:16:34 +0530

    routing added

--- angular-practice/login-page/src/app/app-routing.module.ts
+++ angular-practice/login-page/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SignUpComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'signin', component: SignInComponent },
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- angular-practice/login-page/src/app/app.module.ts
+++ angular-practice/login-page/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    WelcomePageComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
