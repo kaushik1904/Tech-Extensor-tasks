f49bb345a54ea54eba0e915b8297d3fe6fc101ef
kaushik1904 <kaushikprajapati1970@gmail.com>
2023-02-16 16:18:32 +0530

    routing added

--- angular-practice/resto/src/app/app-routing.module.ts
+++ angular-practice/resto/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddRestoComponent } from './add-resto/add-resto.component';
+import { ListRestoComponent } from './list-resto/list-resto.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UpdateRestoComponent } from './update-resto/update-resto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add-resto', component: AddRestoComponent },
+  { path: 'list-resto', component: ListRestoComponent },
+  { path: 'update-resto', component: UpdateRestoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const RoutingComponents = [
+  AddRestoComponent,
+  ListRestoComponent,
+  UpdateRestoComponent,
+  LoginComponent,
+  RegisterComponent,
+];
--- angular-practice/resto/src/app/app.module.ts
+++ angular-practice/resto/src/app/app.module.ts
@@ -3,26 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddRestoComponent } from './add-resto/add-resto.component';
-import { ListRestoComponent } from './list-resto/list-resto.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { UpdateRestoComponent } from './update-resto/update-resto.component';
+import { RoutingComponents } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AddRestoComponent,
-    ListRestoComponent,
-    LoginComponent,
-    RegisterComponent,
-    UpdateRestoComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, RoutingComponents],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
