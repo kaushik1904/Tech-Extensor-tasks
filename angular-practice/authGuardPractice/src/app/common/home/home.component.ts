6afeb1215907a6fe8f19b15b2887e9d3b4dbab0a
kaushik1904 <kaushikprajapati1970@gmail.com>
2023-02-28 15:30:19 +0530

    change home component

--- angular-practice/authGuardPractice/src/app/common/home/home.component.ts
+++ angular-practice/authGuardPractice/src/app/common/home/home.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { ServiceService } from 'src/app/shared/service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
+  constructor(private service: ServiceService) {
+    this.userId = sessionStorage.getItem('id');
+    this.service.getUser(this.userId).subscribe((res) => (this.userData = res));
+  }
+  userId: any;
+  userData: any;
 }
