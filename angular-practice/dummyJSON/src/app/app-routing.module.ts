342e02fa35f2d3f4da20da654564fc8a465d08a8
kaushik1904 <kaushikprajapati1970@gmail.com>
2023-03-02 10:39:57 +0530

    add authguard

--- angular-practice/dummyJSON/src/app/app-routing.module.ts
+++ angular-practice/dummyJSON/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './page/login/login.component';
 import { ProfileComponent } from './page/profile/profile.component';
 import { UserlistComponent } from './page/userlist/userlist.component';
+import { AuthGuard } from './shared/guard/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'userlist', component: UserlistComponent },
-  { path: 'profile', component: ProfileComponent },
+  { path: 'userlist', component: UserlistComponent, canActivate: [AuthGuard] },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: '**', component: LoginComponent },
 ];
 
 @NgModule({
